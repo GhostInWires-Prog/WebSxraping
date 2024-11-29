09cc772cbae9bf8178f779d88a1ef9e0509ddbcb
GhostinWires-Prog <tahirmuqqadam@gmail.com>
2024-11-29 17:59:36 +0500

    wrote server side

--- server3/server.js
+++ server3/server.js
@@ -7,9 +7,28 @@ const app=express()
 
 app.use(cors())// to handle cors Errors
 
-app.get("/api/scape",(req, res)=>
-  {
-
+app.get("/api/scape",async (req, res)=>
+{
+  try {
+    const {data} = await axios.get("https://www.bbc.com/");//Get Data From Server
+    const $ = cheerio.load(data);// Will Load DATA iN cHEERIO
+    const header = [];//Made Array Of Header Later WIll Be Populated
+    const paragraphs = []//Later Will Be Populated WIth paras
+    //Time To Use Cheerio's Object $ For Seprating Header And Paras
+    $('h1,h2,h3,h4,h5,h6').each((_, element) => {
+      header.push($(element).text().trim());
+    })
+    $('p').each((_, element) => {
+      paragraphs.push($(element).text().trim());
+    })
+    //Now both Arrays Holding Values
+    res.json({paragraphs,header})
   }
-)
-
+  catch(error)
+  {
+    console.log(error)
+  }
+});
+app.listen(3000,()=>{{
+  console.log("Server started on port http://localhost3000")
+}});
