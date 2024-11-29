c48e01484aeff2cc5e3bb7dd28541dcc1368fc83
GhostinWires-Prog <tahirmuqqadam@gmail.com>
2024-11-29 18:41:22 +0500

    wrote clientside logic of getting data from server to arrays

--- src/scrape/scrape.component.ts
+++ src/scrape/scrape.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-scrape',
@@ -8,6 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './scrape.component.css'
 })
 export class ScrapeComponent {
+  headers :string[]=[];
+  paragraphs :string[]=[];
+  constructor(private http:HttpClient) {
+    // We Also Can Use Inonit But Will Go With Consturctor
+
+  }
+
+  fetchdata()
+  {
+    this.http.get<{headers: string[],paragrpahs:string[]}>(" http://localhost:3000/api/scape")
+    .subscribe(data=>{
+      this.headers=data.headers;
+      this.paragraphs=data.paragrpahs;
+    },
+    error=>{
+      console.log(error);
+    }
+    )
+
+  }
+
 
 
 }
