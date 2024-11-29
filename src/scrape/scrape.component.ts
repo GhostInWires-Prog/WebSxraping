9ded7ac60152ebd85601994a15c1e21766611028
GhostinWires-Prog <tahirmuqqadam@gmail.com>
2024-11-29 18:11:15 +0500

    Added Scrape Component And Make Them Standalone

---
+++ src/scrape/scrape.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scrape',
+  imports: [],
+  standalone:true,
+  templateUrl: './scrape.component.html',
+  styleUrl: './scrape.component.css'
+})
+export class ScrapeComponent {
+
+
+}
